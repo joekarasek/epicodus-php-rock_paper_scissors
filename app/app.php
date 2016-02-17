1213a745a0816388716c46cd4e20335020e3594b
Joseph Karasek <joseph.karasek@gmail.com>
2016-02-17 08:56:48 -0800

    add message to game to see who won the last round

--- app/app.php
+++ app/app.php
@@ -13,5 +13,28 @@
         ));
     });
 
+    $app->get("/game", function() use ($app) {
+        $my_game = new Game;
+        $winner = $my_game->threeTwoOne($_GET['player-one'], 'rock');
+        if ($winner == 'player 1') {
+            $message_type = 'success';
+            $message_text = 'You have triumphed over computer!';
+        } elseif ($winner == 'player 2') {
+            $message_type = 'danger';
+            $message_text = 'You have been destroyed computer!';
+        } else {
+            $message_type = 'warning';
+            $message_text = 'Draw... the war continues.';
+        }
+
+        return $app['twig']->render('index.html.twig', array(
+            'form' => true,
+            'message' => array(
+                'type' => $message_type,
+                'text' => $message_text
+            )
+        ));
+    });
+
     return $app;
 ?>
